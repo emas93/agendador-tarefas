6fc7a7183d87867a455d9268bdf1b731882642ca
emas93 <eduardamss93@gmail.com>
2026-08-18 18:12:52 -0300

    Ajustes gradle

--- src/main/java/com/emas93/agendadortarefas/AgendadorTarefasApplication.java
+++ src/main/java/com/emas93/agendadortarefas/AgendadorTarefasApplication.java
@@ -2,12 +2,13 @@ package com.emas93.agendadortarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AgendadorTarefasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendadorTarefasApplication.class, args);
 	}
-
 }
