822624d34caf2f3045f67923d374d66d1586bb90
emas93 <eduardamss93@gmail.com>
2026-08-18 16:25:48 -0300

    Commit inicial

---
+++ src/test/java/com/emas93/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emas93.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
